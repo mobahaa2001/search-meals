2c8211cfe67e1f1859f3b5f04b5aa6b5bbfa8445
mobahaa2001 <mobahaa2001@gmail.com>
2023-07-24 09:38:36 +0300

    All-Thing

---
+++ src/components/YouTubeButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <a
+    :href="href"
+    target="_blank"
+    class="px-3 py-2 rounded border-2 text-white border-blue-600 bg-blue-500 hover:bg-blue-600 transition-colors"
+  >
+    <slot>YouTube</slot>
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({
+  href: String,
+});
+</script>
